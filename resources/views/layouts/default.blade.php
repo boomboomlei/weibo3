f97a1bb9df80bb6fce39aaad8c02da988b87c91f
Your Nameboomboomlei <1846352100@qq.com>
2019-08-28 08:05:15 +0000

    查看用户列表

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,6 @@
 <html>
   <head>
-    <title>@yeild('title','weibo lei')-李磊的博客 丢~</title>
+    <title>@yield('title','weibo lei')-李磊的博客 丢~</title>
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
   </head>
   <body>
